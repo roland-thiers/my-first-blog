3c3349000dd1615966ccc35db26fa288d3d79156
roland <roland.thiers@zaclys.net>
2018-12-31 15:14:17 +0400

    new modif add_comment_to_post

--- monblog/blog/views.py
+++ monblog/blog/views.py
@@ -1,9 +1,10 @@
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .forms import PostForm, CommentForm
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
