1389ce2e535b14476c6f23cefa1de64e055658c7
roland <roland.thiers@zaclys.net>
2019-01-07 15:48:59 +0400

    mise en page modifiée

--- monblog/blog/views.py
+++ monblog/blog/views.py
@@ -7,8 +7,13 @@ from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -87,3 +92,8 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+
+
+    
+    
